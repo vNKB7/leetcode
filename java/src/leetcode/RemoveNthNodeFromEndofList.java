96c1c7e1c533b59e72a6b262d29a4a99aed91430
Wendiven <szhjzwz@gmail.com>
2016-04-22 16:10:16 +0800

    19

---
+++ java/src/leetcode/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class RemoveNthNodeFromEndofList {
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		
+		
+		
+		
+		
+		
+		ListNode node = new ListNode(0);
+		return node;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
