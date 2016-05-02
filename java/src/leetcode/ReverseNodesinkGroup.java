d4166103cd12eb827b30c416a8a3396ba99663f1
Wendiven <szhjzwz@gmail.com>
2016-05-02 20:29:50 +0800

    25

---
+++ java/src/leetcode/ReverseNodesinkGroup.java
@@ -0,0 +1,80 @@
+package leetcode;
+
+import leetcode.SwapNodesinPairs.ListNode;
+
+public class ReverseNodesinkGroup {
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode reverseKGroup(ListNode head, int k) {
+		if(head == null)
+			return null;
+		
+		if(k <= 1)
+			return head;
+		
+		
+		ListNode rt = null;
+		
+		ListNode headNode = new ListNode(-1);
+		headNode.next = head;
+		
+		ListNode lastNode = null;
+		
+		while(true){
+			
+			//headNode = headNode;//headNode本身还是上一轮的headNode，因为在上一轮中翻转过了
+			lastNode = headNode;
+			
+			for(int i = 0; i < k; i++){
+				if(lastNode.next == null){
+					if(rt == null)
+						return head;//!!!!
+					else
+						return rt;
+				}else{
+					lastNode = lastNode.next;
+				}
+			}
+			
+			if(rt == null)
+				rt = lastNode;
+			
+			ListNode lastPoint = lastNode;
+			ListNode lastNext = lastNode.next;
+			
+			
+			while(headNode.next != lastPoint){
+				ListNode p = headNode;
+				
+				for(; p.next != lastPoint; p = p.next){
+					
+				}
+				
+				lastPoint.next = p;
+				lastPoint = lastPoint.next;
+			}
+			
+			ListNode tmp = headNode.next;//原本headNode所指向的下一个，这一轮翻转之后的最后一个
+			headNode.next = lastNode;
+			headNode = tmp;
+			
+			lastPoint.next = lastNext;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+
+}
--- java/src/leetcode/SwapNodesinPairs.java
+++ java/src/leetcode/SwapNodesinPairs.java
@@ -1,6 +1,40 @@
 package leetcode;
 
 public class SwapNodesinPairs {
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode swapPairs(ListNode head) {
+		if(head == null){
+			return null;
+		}else if(head.next == null){
+			return head;
+		}
+		
+		ListNode rt = head.next;
+		
+		ListNode curNode = new ListNode(-1);
+		curNode.next = head;
+		
+		while(curNode.next != null && curNode.next.next != null){
+			ListNode n1 = curNode.next;
+			ListNode n2 = n1.next;
+			
+			curNode.next = n2;
+			n1.next = n2.next;
+			n2.next = n1;
+			
+			curNode = n1;
+		}
+		
+		return rt;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
