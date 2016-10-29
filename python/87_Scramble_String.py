4ffe756d859052175d666dd1ebccd79f9796202e
Wendiven <szhjzwz@gmail.com>
2016-10-29 17:37:45 +0800

    87

---
+++ python/87_Scramble_String.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+class Solution(object):
+    def isScramble(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if s1 is None or s2 is None or len(s1) != len(s2):
+            return False
+
+        self.memory = {}
+
+        return self.helper(s1, s2)
+
+
+    def helper(self, s1, s2):
+        if s1 == s2:
+            return True
+
+        if s1 + s2 in self.memory:
+            return self.memory[s1+s2]
+
+        for i in range(1, len(s1)):
+            if self.helper(s1[:i], s2[:i]) and self.helper(s1[i:], s2[i:]) or self.helper(s1[:i], s2[-i:]) and self.helper(s1[i:], s2[:-i]):
+                self.memory[s1+s2] = True
+                return True
+
+        self.memory[s1+s2] = False
+        return False
+
+s = Solution()
+print(s.isScramble('abcd', 'bdac'))
