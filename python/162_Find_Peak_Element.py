15386b431aff774c2b835c64dc41a8ed39e4eeb6
Wendiven <szhjzwz@gmail.com>
2017-01-02 17:06:38 +0800

    to 164

--- python/162_Find_Peak_Element.py
+++ python/162_Find_Peak_Element.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+
+'''
 class Solution(object):
     def findPeakElement(self, nums):
         """
@@ -11,6 +13,36 @@ class Solution(object):
         nums.append(float('-inf'))
 
         for i in range(1, len(nums)-1):
-            if nums[i-1] < nums[i] < nums[i+1]:
+            if nums[i-1] < nums[i] > nums[i+1]:
                 return i-1
+'''
 
+# use binary search
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.insert(0, float('-inf'))
+        nums.append(float('-inf'))
+
+        left = 1
+        right = len(nums) - 1
+
+        while left < right:
+            if right - left == 1:
+                if nums[left-1] < nums[left] > nums[left+1]:
+                    return left - 1
+                else:
+                    return -1
+
+            mid = (left + right) // 2
+            if nums[mid-1] < nums[mid] > nums[mid+1]:
+                return mid - 1
+            elif nums[mid-1] < nums[mid] < nums[mid+1]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return -1
---
+++ python/164_Maximum_Gap.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
